a2a2ceefab8a298c286446f81d5c48b32146a32b
Matt Miles <mtm4440@rit.edu>
2025-10-25 19:55:48 -0400

    This work?

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -7,19 +7,19 @@ const port = 9200;
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:9100",
+  "http://penguinore.net:9100",
+  "http://status.penguinore.net:9100"
+];
+
 // We are sending this back in all responses saying it's allowed to be used by our client
 // If we didn't provide this, the browser would see the origins were different ports and immediately reject it
 // This is like our stamp of approval in the response: "Hey Browser, we allow things to be sent back to port X."
 app.use(
-	cors({
-		origin: `http://localhost:9100`,
-	}),
-	cors({
-		origin: `http://penguinore.net:9100`,
-	}),
-	cors({
-		origin: `http://status.penguinore.net:9100`,
-	})
+  cors({
+    origin: allowedOrigins
+  })
 );
 
 // Does the parsing for the req.body
